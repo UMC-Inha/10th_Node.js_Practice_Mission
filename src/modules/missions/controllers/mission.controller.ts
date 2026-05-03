27ef56b3c2e4912bf9b9e21063f499ff4058678c
rbdus0715 <rbdus0715@gmail.com>
2026-05-04 06:03:45 +0900

    fix: isNaN for storeID

--- src/modules/missions/controllers/mission.controller.ts
+++ src/modules/missions/controllers/mission.controller.ts
@@ -8,17 +8,17 @@ import { STORE_ERROR_CODE } from '../../../common/error-code';
 @injectable()
 export class MissionController {
   constructor(
-    @inject(MissionService) private readonly missionService: MissionService,
+    @inject(MissionService) private readonly missionService: MissionService
   ) {}
 
   public handleCreateMissionForStore = async (req: Request, res: Response) => {
     const storeId = Number(req.params.storeId);
 
-    if (!Number.isInteger(storeId) || storeId <= 0) {
+    if (!Number.isInteger(storeId) || storeId <= 0 || isNaN(storeId)) {
       throw new AppError(
         STORE_ERROR_CODE.STORE_NOT_FOUND,
         `유효하지 않은 가게 id 입니다. (id: ${req.params.storeId})`,
-        StatusCodes.BAD_REQUEST,
+        StatusCodes.BAD_REQUEST
       );
     }
 
