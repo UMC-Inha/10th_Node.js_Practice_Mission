93f4da6c8787fd6385679eba97211f99eb6a7435
Seojin <1020seojin@naver.com>
2026-05-28 00:47:14 +0900

    mission: 9주차 실습 완료

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,15 @@ import path from "path";
 import fs from "fs";
 
 import { RegisterRoutes } from "./generated/routes.js";
+import passport from "passport";
+import { googleStrategy, jwtStrategy } from "./auth.config.js";
+import { ApiResponse } from "./common/responses/api.response.js";
 
 // 환경 변수 설정
 dotenv.config();
+
+passport.use(googleStrategy);
+passport.use(jwtStrategy);
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -19,6 +25,7 @@ app.use(cors()); // cors 방식 허용
 app.use(express.static("public")); // 정적 파일 접근
 app.use(express.json()); // request의 본문을 json으로 해석할 수 있도록 함(JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
+app.use(passport.initialize());
 
 // Swagger 설정
 const swaggerFile = JSON.parse(
@@ -38,6 +45,22 @@ app.use(
 const router = express.Router();
 RegisterRoutes(router); 
 app.use("/api/v1", router);
+
+const isLogin = passport.authenticate("jwt", {
+  session: false,
+});
+
+app.get("/mypage", isLogin, (req, res) => {
+  return res.status(200).json(
+    ApiResponse.success(
+      200,
+      `인증 성공! ${(req.user as any).name}님의 마이페이지입니다.`,
+      {
+        user: req.user,
+      }
+    )
+  );
+});
 
 // 서버 시작
 app.listen(port, () => {
